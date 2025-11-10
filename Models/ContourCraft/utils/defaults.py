f0bec24485e4b372fdc50cddbf770d97ddd8c414
JanRojc <jan.rojc@outfit7.com>
2025-11-10 15:39:43 +0100

    moved contourcraft folder

--- Models/ContourCraft/utils/defaults.py
+++ Models/ContourCraft/utils/defaults.py
@@ -10,13 +10,13 @@ DEFAULTS = dict()
 DEFAULTS['project_name'] = 'ccraft'
 
 
-DEFAULTS['data_root'] = '/Users/jan.rojc/Documents/MagCode/desktop/data/ccraft_data/'
+DEFAULTS['data_root'] = '/Users/jan.rojc/Documents/MagCode/Data/ccraft_data/'
 DEFAULTS['aux_data'] = os.path.join(DEFAULTS['data_root'], 'aux_data')
 DEFAULTS['project_dir'] = '/Users/jan.rojc/Documents/MagCode/'
-DEFAULTS['config_dir'] = '/Users/jan.rojc/Documents/MagCode/desktop/ContourCraft/configs/'
+DEFAULTS['config_dir'] = '/Users/jan.rojc/Documents/MagCode/Models/ContourCraft/configs/'
 
 DEFAULTS['experiment_root'] = os.path.join(DEFAULTS['data_root'], 'experiments')
 
-DEFAULTS['CMU_root'] = '/Users/jan.rojc/Documents/MagCode/desktop/data/AMASS/smpl/CMU/'
+DEFAULTS['CMU_root'] = '/Users/jan.rojc/Documents/MagCode/Data/AMASS/smpl/CMU/'
 
 DEFAULTS = munchify(DEFAULTS)
