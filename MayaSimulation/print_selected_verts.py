e39318fe35f93d629d13a100237c20e774ffb423
JanRojc <jan.rojc@outfit7.com>
2025-12-08 12:58:46 +0100

    updated maya print selection script

--- MayaSimulation/print_selected_verts.py
+++ MayaSimulation/print_selected_verts.py
@@ -1,14 +1,14 @@
 import maya.cmds as cmds
 
 sel = cmds.ls(selection=True)
-nodes = []
-for node in sel:
-    node_tuple = node[12:-1]
-    if ":" in node_tuple:
-        i = int(node_tuple.split(":")[0])
-        j = int(node_tuple.split(":")[1])
-        nodes += [x for x in range(i, j+1)]
+verts = []
+for vert in sel:
+    vert = vert[12:-1]
+    if ":" in vert:
+        i = int(vert.split(":")[0])
+        j = int(vert.split(":")[1])
+        verts += [x for x in range(i, j+1)]
     else:
-        nodes.append(int(node_tuple))
-print("Selected nodes:", nodes)
+        verts.append(int(vert))
+print("Selected verts:", verts)
 
